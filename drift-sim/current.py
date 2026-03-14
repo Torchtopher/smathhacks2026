9357e3098c19e1cbd55e877b5b0707ac527964fd
Gandore <2kidactive@gmail.com>
2026-03-14 16:35:54 -0400

    Prediction function

--- drift-sim/current.py
+++ drift-sim/current.py
@@ -2,23 +2,25 @@ from opendrift.models.oceandrift import OceanDrift
 from opendrift.readers import reader_netCDF_CF_generic
 from datetime import datetime, timedelta
 
-o = OceanDrift(loglevel=20)
-
-# Uses old data, not sure the exact dates
 reader = reader_netCDF_CF_generic.Reader(
-    'https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0')
-
-o.add_reader([reader])
-
-o.seed_elements(
-    # Off the coast of NC
-    lat=34.7657861111,
-    lon=-75.7673472222,
-    number=20,
-    radius=1000,
-    time=datetime(2023,1,1)
+    'https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0'
 )
 
-# Calculate position over 2 days
-o.run(duration=timedelta(days=2), time_step=900)
-o.animation()
+def predict(start_t, dt, lat, lon):
+    o = OceanDrift(loglevel=20)
+    o.add_reader([reader])
+    o.seed_elements(
+        lat=lat,
+        lon=lon,
+        number=1,
+        radius=0,
+        time=start_t
+    )
+
+    o.run(duration=dt, time_step=dt)
+    print(o.elements.lon)
+    print(o.elements.lat)
+
+while True:
+    days = int(input("Enter the number of days: "))
+    predict(datetime(2024, 1, 1), timedelta(days=days), lat=35, lon=-74)
