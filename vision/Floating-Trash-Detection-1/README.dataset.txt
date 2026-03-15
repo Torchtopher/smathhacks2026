6af5f60c26cac778b54bfe61887dd89a21f2a7a8
Evan Kim <evanhkim09@gmail.com>
2026-03-15 03:31:04 -0400

    Some cleanup

--- vision/Floating-Trash-Detection-1/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Floating Trash Detection > 2024-07-22 5:40pm
-https://universe.roboflow.com/waterqualityprediction/floating-trash-detection
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- vision/download.py
+++
@@ -1,6 +0,0 @@
-from roboflow import Roboflow
-
-# rf = Roboflow(api_key="API KEY")
-project = rf.workspace("waterqualityprediction").project("floating-trash-detection")
-version = project.version(1)
-version.download("yolo26")
